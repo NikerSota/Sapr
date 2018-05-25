4df4fd6a76ed2759cdcb0e35cdee95f6c13302a0
NikerSota <39484103+NikerSota@users.noreply.github.com>
2018-05-26 01:38:54 +0300

    Добавил учитывание углов при построении
    Теперь эта программа решает еще одно задание по сопру, с косыми усилиями

--- Sapr/Sapr/Form2.cs
+++ Sapr/Sapr/Form2.cs
@@ -22,30 +22,78 @@ namespace Sapr
         class Niker_class_raschet
         {
 
-            public float P1 = 0, P2 = 0, M1 = 0, M2 = 0, Q1 = 0, Q2 = 0; // ВЕЛЕЧИНЫ. ВВОДЯТСЯ ВРУЧНУЮ В ФОРМЕ
-            public float UP1 = 0, UP2 = 0; //УГОЛ, ВВОДИТСЯ ВРУЧНУЮ
-            public float PP1 = 0, PP2 = 0, PM1 = 0, PM2 = 0, PQ1 = 0, PQ2 = 0, PB = 0; //ПОЛОЖЕНИЕ, ВВОДИТСЯ ВРУЧНУЮ
-            public float Q, PQ; // НАХОЖДЕНИЕ РАВНОДЕЙСТВУЮЩЕЙ НАГРУЗКИ
-            public float RA, RB;  // РЕАКЦИИ ОПОР, РАСЧИТЫВАЮТСЯ В ФОРМУЛАХ
-            public string otv, rat, rbt; //ОТВЕТ, ВЕКТОР А ВВЕРХ/ВНИЗБ ВЕКТОР В ВВЕРХ/ВНИЗ
-            public bool RAS = false, RBS = false; // СТАТУС РЕАКЦИЙ (ЕСЛИ ЛОЖЬ - ВЕКТОР РЕАКЦИИ ВВЕРХ)
+            public double P1_F = 0, P2_F = 0, M1_F = 0, M2_F = 0, Q1_F = 0, Q2_F = 0; // ВЕЛЕЧИНЫ. ВВОДЯТСЯ ВРУЧНУЮ В ФОРМЕ
+            public double P1_U = 0, P2_U = 0; //УГОЛ, ВВОДИТСЯ ВРУЧНУЮ
+            public double P1_X = 0, P1_Y = 0; //УГОЛ, ВВОДИТСЯ ВРУЧНУЮ
+            public double P2_X = 0, P2_Y = 0; //УГОЛ, ВВОДИТСЯ ВРУЧНУЮ
+            public double P1_L = 0, P2_L = 0, M1_L = 0, M2_L = 0, Q1_L = 0, Q2_L = 0, B_L = 0; //ПОЛОЖЕНИЕ, ВВОДИТСЯ ВРУЧНУЮ
+            public double Q_F, Q_L; // НАХОЖДЕНИЕ РАВНОДЕЙСТВУЮЩЕЙ НАГРУЗКИ
+            public double VA, VB;  // РЕАКЦИИ ОПОР, РАСЧИТЫВАЮТСЯ В ФОРМУЛАХ
+            public string otv, VA_S_V, VB_S_V; //ОТВЕТ, ВЕКТОР А ВВЕРХ/ВНИЗБ ВЕКТОР В ВВЕРХ/ВНИЗ
+            public bool VA_Z = false, VB_Z = false; // СТАТУС РЕАКЦИЙ (ЕСЛИ ЛОЖЬ - ВЕКТОР РЕАКЦИИ ВВЕРХ)
 
+            public void P_raschet()
+            {
+                if (P1_U >= 0 && P1_U <= 90)
+                {
+                    P1_X = Math.Sin(P1_U * Math.PI / 180)*P1_F; //ЕСЛИ В ПРЕДЕЛАХ 90 ГРАДУСОВ, 
+                    P1_Y = Math.Cos(P1_U * Math.PI / 180) * P1_F;
+                }
+                else if (P1_U >= 180 && P1_U <= 270)
+                {
+                    P1_X = Math.Sin(P1_U * Math.PI / 180) * P1_F;
+                    P1_Y = Math.Cos(P1_U * Math.PI / 180) * P1_F;
+                }
+                else if (P1_U >= 90 && P1_U <= 180)
+                {
+                    P1_X = Math.Cos(P1_U * Math.PI / 180) * P1_F;
+                    P1_Y = Math.Sin(P1_U * Math.PI / 180) * P1_F;
+                }
+                else if (P1_U >= 270 && P1_U <= 360)
+                {
+                    P1_X = Math.Cos(P1_U * Math.PI / 180) * P1_F;
+                    P1_Y = Math.Sin(P1_U * Math.PI / 180) * P1_F;
+                }
+
+
+
+                if (P2_U >= 0 && P2_U <= 90)
+                {
+                    P2_X = Math.Sin(P2_U * Math.PI / 180) * P2_F;
+                    P2_Y = Math.Cos(P2_U * Math.PI / 180) * P2_F;
+                }
+                else if (P2_U >= 180 && P2_U <= 270)
+                {
+                    P2_X = Math.Cos(P2_U * Math.PI / 180) * P2_F;
+                    P2_Y = Math.Sin(P2_U * Math.PI / 180) * P2_F;
+                }
+                else if (P2_U >= 90 && P2_U <= 180)
+                {
+                    P2_X = Math.Sin(P2_U * Math.PI / 180) * P2_F;
+                    P2_Y = Math.Cos(P2_U * Math.PI / 180) * P2_F;
+                }
+                else if (P2_U >= 270 && P2_U <= 360)
+                {
+                    P2_X = Math.Cos(P2_U * Math.PI / 180) * P2_F;
+                    P2_Y = Math.Sin(P2_U * Math.PI / 180) * P2_F;
+                }
+            }
             public void Q_raschet()
             {
-                if (Q1 == Q2)
+                if (Q1_F == Q2_F)
                 {
-                    Q = Q1 * (PQ2 - PQ1); //ДЛЯ ПРЯМОУГОЛЬНОЙ НАГРУЗКИ
-                    PQ = PQ1 + (PQ2 - PQ1) / 2; //ТОЧКА ПРИЛОЖЕНИЯ СИЛЫ
+                    Q_F = Q1_F * (Q2_L - Q1_L); //ДЛЯ ПРЯМОУГОЛЬНОЙ НАГРУЗКИ
+                    Q_L = Q1_L + (Q2_L - Q1_L) / 2; //ТОЧКА ПРИЛОЖЕНИЯ СИЛЫ
                 }
-                else if (Q1 == 0) // ДЛЯ ПРЯМО-ТРЕУГОЛЬНОЙ ВОЗРАСТАЮЩЕЙ НАГРУЗКИ 
+                else if (Q1_F == 0) // ДЛЯ ПРЯМО-ТРЕУГОЛЬНОЙ ВОЗРАСТАЮЩЕЙ НАГРУЗКИ 
                 {
-                    Q = (PQ2 - PQ1) * Q2 / 2;
-                    PQ = PQ1 + 2 * (PQ2 - PQ1) / 3;
+                    Q_F = (Q2_L - Q1_L) * Q2_F / 2;
+                    Q_L = Q1_L + 2 * (Q2_L - Q1_L) / 3;
                 }
-                else if (Q2 == 0) // ДЛЯ ПРЯМО-ТРЕУГОЛЬНОЙ СПАДАЮЩЕЙ НАГРУЗКИ 
+                else if (Q2_F == 0) // ДЛЯ ПРЯМО-ТРЕУГОЛЬНОЙ СПАДАЮЩЕЙ НАГРУЗКИ 
                 {
-                    Q = (PQ2 - PQ1) * Q1 / 2;
-                    PQ = PQ1 + 1 * (PQ2 - PQ1) / 3;
+                    Q_F = (Q2_L - Q1_L) * Q1_F / 2;
+                    Q_L = Q1_L + 1 * (Q2_L - Q1_L) / 3;
                 }
                 //else if (Q1 > Q2) //ПРОДОЛЖЕНИЕ ДЛЯ ТРАПЕЦЕВИДНОЙ НАГРУЗКИ
                 //{
@@ -55,41 +103,41 @@ namespace Sapr
                 //object.Method1();
                 //}
             }
-            public void RB_raschet() // РАСЧЕТ ОПОРЫ В
+            public void VB_raschet() // РАСЧЕТ ОПОРЫ В
             {
                 Q_raschet();
-                M1 = -M1;
-                RB = (M1 - P1 * PP1 + P2 * PP2 + Q * PQ + M2) / PB; // РАСЧЕТ РЕАКЦИИ А ПУТЕМ НАОЖДЕНИЯ СУММЫ МОМЕНТОВ
-              //RB = (10 - 15 * 2 + 15....) / 8
-                if (RB < 0)
+                P_raschet();
+                M1_F = -M1_F;
+                VB = (M1_F + P1_Y * P1_L + P2_Y * P2_L + Q_F * Q_L + M2_F) / B_L; // РАСЧЕТ РЕАКЦИИ А ПУТЕМ НАОЖДЕНИЯ СУММЫ МОМЕНТОВ
+                                                                                  //VB = (10 - 15 * 2 + 15....) / 8
+                if (VB < 0)
                 {
-                    RB = -RB; // ЗНАЧЕНИЕ В ПО МОДУЛЮ
-                    RBS = !RBS; // ПОМЕНЯТЬ НАПРАВЛЕНИЕ ВЕКТОРА
+                    VB = -VB; // ЗНАЧЕНИЕ В ПО МОДУЛЮ
+                    VB_Z = !VB_Z; // ПОМЕНЯТЬ НАПРАВЛЕНИЕ ВЕКТОРА
                 }
-                //return RB; //ВЫВОД ПОСЧИТАННОЙ РЕАКЦИИ В
+                //return VB; //ВЫВОД ПОСЧИТАННОЙ РЕАКЦИИ В
             }
-            public void RA_raschet() // РАСЧЕТ ОПОРЫ А
+            public void VA_raschet() // РАСЧЕТ ОПОРЫ А
             {
-                Q_raschet();
-                RB_raschet();
-                M1 = -M1;
-                RA = -P1 + P2 + Q - RB; // РАСЧЕТ РЕАКЦИИ В ПУТЕМ НАХОЖДЕНИЯ СУМЫ ВЕРТИКАЛЬНЫХ СИЛ
-                if (RA < 0)
+                VB_raschet();
+                M1_F = -M1_F;
+                VA = P1_Y + P2_Y + Q_F - VB; // РАСЧЕТ РЕАКЦИИ В ПУТЕМ НАХОЖДЕНИЯ СУМЫ ВЕРТИКАЛЬНЫХ СИЛ
+                if (VA < 0)
                 {
-                    RA = -RA; // ЗНАЧЕНИЕ А ПО МОДУЛЮ
-                    RAS = !RAS; // ПОМЕНЯТЬ НАПРАВЛЕНИЕ ВЕКТОРА
+                    VA = -VA; // ЗНАЧЕНИЕ А ПО МОДУЛЮ
+                    VA_Z = !VA_Z; // ПОМЕНЯТЬ НАПРАВЛЕНИЕ ВЕКТОРА
                 }
-                //return RA; //ВЫВОД ПОСЧИТАНОЙ РЕАКЦИИ А
+                //return VA; //ВЫВОД ПОСЧИТАНОЙ РЕАКЦИИ А
             }
             public void Vektory()
             {
-                RA_raschet();
+                VA_raschet();
 
-                if (RBS == true) rbt = "Вектор RB направлен вниз";
-                else rbt = "Вектор RB направлен вверх";
-                if (RAS == true) rat = "Вектор RA направлен вниз";
-                else rat = "Вектор RA направлен вверх";
-                otv = "Вертикальная реакция опоры А = " + Math.Round(RA, 2) + ". " + rat + '\n' + "Вертикальная реакция опоры B = " + Math.Round(RB, 2) + ". " + rbt;
+                if (VB_Z == true) VB_S_V = "Вектор VB направлен вниз";
+                else VB_S_V = "Вектор VB направлен вверх";
+                if (VA_Z == true) VA_S_V = "Вектор VA направлен вниз";
+                else VA_S_V = "Вектор VA направлен вверх";
+                otv = "Вертикальная реакция опоры А = " + Math.Round(VA, 2) + ". " + VA_S_V + '\n' + "Вертикальная реакция опоры B = " + Math.Round(VB, 2) + ". " + VB_S_V;
                 // Запись ответа
                 //RAS = false;
                 //RBS = false;
@@ -128,15 +176,15 @@ namespace Sapr
         void Baka_graphic()
         {
 
-        //float P1X = 2, P2X = 3, Q2X = 6, RBX = 8; // ПАРАМЕТРЫ РАССТОЯНИЙ
-        //float RAY = -0.9F, P1Y = 15F, P2Y = -10F, Q1Y = 5F, Q2Y = 5F, RBY = -10.9F;// ПАРАМЕТРЫ СИЛ
-        //int[] arrow = new int[11]; // МАССИВ ДАННЫХ, ИСПОЛЬЗУЮЩИЙСЯ ДЛЯ ВВОБДА ИНФОРМАЦИИ
-        //Pen beamPen = new Pen(Color.Black, 2); // СОЗДАЕМ КИСТЬ, СТАВИМ ЦВЕТ
-        //Pen grafPen = new Pen(Color.Red, 1);  // СОЗДАЕМ КИСТЬ, СТАВИМ ЦВЕТ
-        //RAY *= 10; P1Y *= 10; P2Y *= 10; Q1Y *= 10; Q2Y *= 10; RBY *= 10;
-        //P1X *= 80; P2X *= 80; Q2X *= 80; RBX *= 80;
-        PointF[] grafQDraw = //МАССИВ ДЛЯ ПОСТРОЕНИЯ "СТРЕЛКИ" 
-    {
+            //float P1X = 2, P2X = 3, Q2X = 6, RBX = 8; // ПАРАМЕТРЫ РАССТОЯНИЙ
+            //float RAY = -0.9F, P1Y = 15F, P2Y = -10F, Q1Y = 5F, Q2Y = 5F, RBY = -10.9F;// ПАРАМЕТРЫ СИЛ
+            //int[] arrow = new int[11]; // МАССИВ ДАННЫХ, ИСПОЛЬЗУЮЩИЙСЯ ДЛЯ ВВОБДА ИНФОРМАЦИИ
+            //Pen beamPen = new Pen(Color.Black, 2); // СОЗДАЕМ КИСТЬ, СТАВИМ ЦВЕТ
+            //Pen grafPen = new Pen(Color.Red, 1);  // СОЗДАЕМ КИСТЬ, СТАВИМ ЦВЕТ
+            //RAY *= 10; P1Y *= 10; P2Y *= 10; Q1Y *= 10; Q2Y *= 10; RBY *= 10;
+            //P1X *= 80; P2X *= 80; Q2X *= 80; RBX *= 80;
+            PointF[] grafQDraw = //МАССИВ ДЛЯ ПОСТРОЕНИЯ "СТРЕЛКИ" 
+        {
             //new PointF(15F, 150F), // отправная 
             //new PointF(15F, 150F + RAY),
             //new PointF(15F + P1X, 150F + RAY),
@@ -149,19 +197,19 @@ namespace Sapr
             //new PointF(15F + RBX, 150F) // КОНЕЧНАЯ 
         };
 
-        //Graphics gPanel = panel1.CreateGraphics(); //СОЗДАНИЕ ГРАФИЧЕСКОГО ИНТЕРФЕЙСЯ В ЭЛЕМЕНТЕ ПАНЕЛЬ
-        //gPanel.DrawRectangle(beamPen, 0, 0, 700, 300); // РАМКА ГРАФИКА
-        //gPanel.DrawLine(beamPen, 10, 10, 10, 290); // ВЕРТИКАЛЬ
-        //gPanel.DrawLine(beamPen, 10, 150, 690, 150); // ГОРИЗОНТАЛЬ
-        //gPanel.DrawLines(grafPen, grafQDraw); // ГРАФИК КЬЮ
-        //gPanel.DrawBezier(grafPen, 20F, 20F, 40F, 60F, 60F, 40F, 100F, 100F); // ПРИМЕР КРИВОЙ
-        //Graphics gButton = button1.CreateGraphics(); //МАКРОС ДЛЯ КНОПКИ
-        ////label1.Text = "lox"; // ПРИСВОЕНИЕ ТЕКСТУ
-        ////label1.Location = { }; // ПРИСВОЕНИЕ ЛОКАЦИИ
+            //Graphics gPanel = panel1.CreateGraphics(); //СОЗДАНИЕ ГРАФИЧЕСКОГО ИНТЕРФЕЙСЯ В ЭЛЕМЕНТЕ ПАНЕЛЬ
+            //gPanel.DrawRectangle(beamPen, 0, 0, 700, 300); // РАМКА ГРАФИКА
+            //gPanel.DrawLine(beamPen, 10, 10, 10, 290); // ВЕРТИКАЛЬ
+            //gPanel.DrawLine(beamPen, 10, 150, 690, 150); // ГОРИЗОНТАЛЬ
+            //gPanel.DrawLines(grafPen, grafQDraw); // ГРАФИК КЬЮ
+            //gPanel.DrawBezier(grafPen, 20F, 20F, 40F, 60F, 60F, 40F, 100F, 100F); // ПРИМЕР КРИВОЙ
+            //Graphics gButton = button1.CreateGraphics(); //МАКРОС ДЛЯ КНОПКИ
+            ////label1.Text = "lox"; // ПРИСВОЕНИЕ ТЕКСТУ
+            ////label1.Location = { }; // ПРИСВОЕНИЕ ЛОКАЦИИ
 
-    }
+        }
 
-    private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
             Form fff = new Form3();
             fff.Show();
@@ -175,23 +223,23 @@ namespace Sapr
         private void button3_Click(object sender, EventArgs e)
         {
             Niker_class_raschet Vivid = new Niker_class_raschet();
-            Vivid.P1 = (Convert.ToInt16(TXBX_P1_F.Text));
-            Vivid.P2 = (Convert.ToInt16(TXBX_P2_F.Text));
-            Vivid.M1 = (Convert.ToInt16(TXBX_M1_F.Text));
-            Vivid.M2 = (Convert.ToInt16(TXBX_M2_F.Text));
-            Vivid.Q1 = (Convert.ToInt16(TXBX_Q1_F.Text));
-            Vivid.Q2 = (Convert.ToInt16(TXBX_Q2_F.Text));
+            Vivid.P1_F = (Convert.ToInt16(TXBX_P1_F.Text));
+            Vivid.P2_F = (Convert.ToInt16(TXBX_P2_F.Text));
+            Vivid.M1_F = (Convert.ToInt16(TXBX_M1_F.Text));
+            Vivid.M2_F = (Convert.ToInt16(TXBX_M2_F.Text));
+            Vivid.Q1_F = (Convert.ToInt16(TXBX_Q1_F.Text));
+            Vivid.Q2_F = (Convert.ToInt16(TXBX_Q2_F.Text));
 
-            Vivid.PP1 = (Convert.ToInt16(TXBX_P1_L.Text));
-            Vivid.PP2 = (Convert.ToInt16(TXBX_P2_L.Text));
-            Vivid.PM1 = (Convert.ToInt16(TXBX_M1_L.Text));
-            Vivid.PM2 = (Convert.ToInt16(TXBX_M2_L.Text));
-            Vivid.PQ1 = (Convert.ToInt16(TXBX_Q1_L.Text));
-            Vivid.PQ2 = (Convert.ToInt16(TXBX_Q2_L.Text));
-            Vivid.PB = (Convert.ToInt16(TXBX_B_L.Text));
+            Vivid.P1_L = (Convert.ToInt16(TXBX_P1_L.Text));
+            Vivid.P2_L = (Convert.ToInt16(TXBX_P2_L.Text));
+            Vivid.M1_L = (Convert.ToInt16(TXBX_M1_L.Text));
+            Vivid.M2_L = (Convert.ToInt16(TXBX_M2_L.Text));
+            Vivid.Q1_L = (Convert.ToInt16(TXBX_Q1_L.Text));
+            Vivid.Q2_L = (Convert.ToInt16(TXBX_Q2_L.Text));
+            Vivid.B_L = (Convert.ToInt16(TXBX_B_L.Text));
 
-            Vivid.UP1 = (Convert.ToInt16(TXBX_P1_U.Text));
-            Vivid.UP2 = (Convert.ToInt16(TXBX_P2_U.Text));
+            Vivid.P1_U = (Convert.ToInt16(TXBX_P1_U.Text));
+            Vivid.P2_U = (Convert.ToInt16(TXBX_P2_U.Text));
 
             Vivid.Vektory();
 
