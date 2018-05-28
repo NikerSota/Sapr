987b6c512552d59ccb0ce80ac8e31a558fde751d
Minato Uzumake <39523511+youngt1tan@users.noreply.github.com>
2018-05-28 18:26:32 +0300

    kod balki v Form3

--- Sapr/Sapr/Form3.Designer.cs
+++ Sapr/Sapr/Form3.Designer.cs
@@ -28,23 +28,170 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
-			this.SuspendLayout();
-			// 
-			// Form3
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackgroundImage = global::Sapr.Properties.Resources._715842e401125b9;
-			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.ClientSize = new System.Drawing.Size(556, 388);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "Form3";
-			this.Text = "SupportBeam - График";
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(697, 199);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(261, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(302, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(343, 328);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(384, 328);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(425, 328);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(466, 328);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(507, 328);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(548, 328);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 11;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(595, 328);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 12;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Sapr.Properties.Resources._715842e401125b9;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(721, 378);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form3";
+            this.Text = "SupportBeam - График";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
 }
--- Sapr/Sapr/Form3.cs
+++ Sapr/Sapr/Form3.cs
@@ -16,5 +16,284 @@ namespace Sapr
 		{
 			InitializeComponent();
 		}
-	}
+        void arrowdrow() // ФУНКЦИЯ РИСОВАНИЯ БАЛКИ И СТРЕЛКИ
+        {
+            int XAR = 8; XAR *= 80; // ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ СТРЕЛКИ СИЛЫ
+            int MaxL = 650; //МАКСИМАЛЬНАЯ ДЛИНА БАЛКИ
+            int MaxF = XAR; // ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ РАЗМЕРА СТРЕЛКИ СИЛЫ 
+            int showF = 15; showF *= 80; // ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ ЗНАЧЕНИЯ СТРЕЛКИ СИЛЫ
+            int F = showF; // ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ РАЗМЕРА ЗНАЧЕНИЯ СТРЕЛКИ СИЛЫ 
+
+            int XAR2 = 2; XAR2 *= 80; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ ВТОРОЙ СТРЕЛКИ СИЛЫ
+            int MaxF2 = XAR2; // ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ РАЗМЕРА ВТОРОЙ СТРЕЛКИ СИЛЫ 
+            int showF2 = 12; showF2 *= 80; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ ЗНАЧЕНИЯ ВТОРОЙ СТРЕЛКИ СИЛЫ
+            int F2 = showF2; // ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ РАЗМЕРА ЗНАЧЕНИЯ ВТОРОЙ СТРЕЛКИ СИЛЫ
+
+
+            int XAR3 = 0; XAR3 *= 80; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ СИЛЫ МОМЕНТА
+            int MaxM = XAR3; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ РАЗМЕРА СИЛЫ МОМЕНТА
+            int showM = 11; showM *= 80; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ ЗНАЧЕНИЯ СИЛЫ МОМЕНТА
+            int M = showM; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ  РАЗМЕРА ЗНАЧЕНИЯ СИЛЫ МОМЕНТА
+
+            int XAR4 = 6; XAR4 *= 80; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ  СИЛЫ МОМЕНТА (2)
+            int MaxM2 = XAR4; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ РАЗМЕРА СИЛЫ МОМЕНТА (2)
+            int showM2 = 17; showM2 *= 80; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ ЗНАЧЕНИЯ  СИЛЫ МОМЕНТА (2)
+            int M2 = showM2; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ РАЗМЕРА ЗНАЧЕНИЯ СИЛЫ МОМЕНТА (2)
+
+            int XAR5 = 6; XAR5 *= 80; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ НАГРУЗКИ 
+            int MaxQ = XAR5; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ РАЗМЕРА НАГРУЗКИ
+            int showQ = 13; showQ *= 80; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ ЗНАЧЕНИЯ НАГРУЗКИ 
+            int Q = showQ; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ  РАЗМЕРА ЗНАЧЕНИЯ НАГРУЗКИ
+
+
+            int XAR6 = 0; XAR6 *= 80; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ  ОПОРЫ 
+            int MaxO = XAR6; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ РАЗМЕРА ОПОРЫ
+
+            int XAR7 = 8; XAR7 *= 80; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ  2 ОПОРЫ 
+            int MaxO2 = XAR7; //ПЕРЕМЕННАЯ, ОТВЕЧАЮЩАЯ ЗА ПОЛОЖЕНИЕ РАЗМЕРА 2 ОПОРЫ
+
+            int[] arrow = new int[11]; // МАССИВ ДАННЫХ, ИСПОЛЬЗУЮЩИЙСЯ ДЛЯ ВВОБДА ИНФОРМАЦИИ
+            Pen p = new Pen(Color.Red, 1); // СОЗДАЕМ КИСТЬ, СТАВИМ ЦВЕТ
+            PointF[] arrowpoints = //МАССИВ ДЛЯ ПОСТРОЕНИЯ "СТРЕЛКИ" 
+        {
+            new PointF(5F+XAR, 60F),
+            new PointF(10F+XAR, 65F),
+            new PointF(10F+XAR, 5F),
+            new PointF(10+XAR, 65F),
+            new PointF(15F+XAR, 60F)
+        };
+            PointF[] arrowpoints2 = //МАССИВ ДЛЯ ПОСТРОЕНИЯ "ВТОРОЙ СТРЕЛКИ" 
+{
+            new PointF(5F+XAR2, 70F),
+            new PointF(10F+XAR2, 65F),
+            new PointF(10F+XAR2, 110F),
+            new PointF(10+XAR2, 65F),
+            new PointF(15F+XAR2, 70F)
+        };
+            PointF[] arrowpoints3 = //МАССИВ ДЛЯ ПОСТРОЕНИЯ "СИЛЫ МОМЕНТА" 
+{
+            new PointF(15F+XAR3, 15F),
+            new PointF(20F+XAR3, 20F),
+            new PointF(15F+XAR3, 25F),
+            new PointF(20F+XAR3, 20F),
+            new PointF(10F+XAR3, 20F),
+            new PointF(10F+XAR3, 95F),
+            new PointF(0F+XAR3, 95F),
+            new PointF(5F+XAR3, 90F),
+            new PointF(0F+XAR3, 95F),
+            new PointF(5F+XAR3, 100F)
+
+
+        };
+            PointF[] arrowpoints4 = //МАССИВ ДЛЯ ПОСТРОЕНИЯ "СИЛЫ МОМЕНТА(2)" 
+{
+            new PointF(15F+XAR4, 30F),
+            new PointF(20F+XAR4, 35F),
+            new PointF(15F+XAR4, 40F),
+            new PointF(20F+XAR4, 35F),
+            new PointF(10F+XAR4, 35F),
+            new PointF(10F+XAR4, 95F),
+            new PointF(0F+XAR4, 95F),
+            new PointF(5F+XAR4, 90F),
+            new PointF(0F+XAR4, 95F),
+            new PointF(5F+XAR4, 100F)
+
+
+
+        };
+            PointF[] arrowpoints5 = //МАССИВ ДЛЯ ПОСТРОЕНИЯ "НАГРУЗКИ" 
+{
+            new PointF(-230F+XAR5, 65F),
+            new PointF(-230F+XAR5, 50F),
+            new PointF(-210F+XAR5, 50F),
+            new PointF(-210F+XAR5, 65F),
+            new PointF(-210F+XAR5, 50F),
+            new PointF(-190F+XAR5, 50F),
+            new PointF(-190F+XAR5, 65F),
+            new PointF(-190F+XAR5, 50F),
+            new PointF(-170F+XAR5, 50F),
+            new PointF(-170F+XAR5, 65F),
+            new PointF(-170F+XAR5, 50F),
+            new PointF(-150F+XAR5, 50F),
+            new PointF(-150F+XAR5, 65F),
+            new PointF(-150F+XAR5, 50F),
+            new PointF(-130F+XAR5, 50F),
+            new PointF(-130F+XAR5, 65F),
+            new PointF(-130F+XAR5, 50F),
+            new PointF(-110F+XAR5, 50F),
+            new PointF(-110F+XAR5, 65F),
+            new PointF(-110F+XAR5, 50F),
+            new PointF(-90F+XAR5, 50F),
+            new PointF(-90F+XAR5, 65F),
+            new PointF(-90F+XAR5, 50F),
+            new PointF(-70F+XAR5, 50F),
+            new PointF(-70F+XAR5, 65F),
+            new PointF(-70F+XAR5, 50F),
+            new PointF(-50F+XAR5, 50F),
+            new PointF(-50F+XAR5, 65F),
+            new PointF(-50F+XAR5, 50F),
+            new PointF(-30F+XAR5, 50F),
+            new PointF(-30F+XAR5, 65F),
+            new PointF(-30F+XAR5, 50F),
+            new PointF(-10F+XAR5, 50F),
+            new PointF(-10F+XAR5, 65F),
+            new PointF(-10F+XAR5, 50F),
+            new PointF(10F+XAR5, 50F),
+            new PointF(10F+XAR5, 65F)
+
+
+        };
+
+            PointF[] arrowpoints6 = //МАССИВ ДЛЯ ПОСТРОЕНИЯ "ОПОРЫ)" 
+{
+            new PointF(3F+XAR6, 75F),
+            new PointF(17F+XAR6, 75F),
+            new PointF(20F+XAR6, 75F),
+            new PointF(17F+XAR6, 75F),
+            new PointF(10F+XAR6, 65F),
+            new PointF(3F+XAR6, 75F),
+            new PointF(0F+XAR6, 75F),
+            new PointF(3F+XAR6, 75F),
+            new PointF(4F+XAR6, 80F),
+            new PointF(5F+XAR6, 75F),
+            new PointF(7F+XAR6, 75F),
+            new PointF(6F+XAR6, 80F),
+            new PointF(7F+XAR6, 75F),
+            new PointF(9F+XAR6, 75F),
+            new PointF(8F+XAR6, 80F),
+            new PointF(9F+XAR6, 75F),
+            new PointF(11F+XAR6, 75F),
+            new PointF(10F+XAR6, 80F),
+            new PointF(11F+XAR6, 75F),
+            new PointF(13F+XAR6, 75F),
+            new PointF(12F+XAR6, 80F),
+            new PointF(13F+XAR6, 75F),
+            new PointF(15F+XAR6, 75F),
+            new PointF(14F+XAR6, 80F)
+
+
+        };
+
+            PointF[] arrowpoints71 = //МАССИВ ДЛЯ ПОСТРОЕНИЯ "2 ОПОРЫ" 
+{
+            new PointF(3F+XAR7, 75F),
+            new PointF(17F+XAR7, 75F),
+            new PointF(20F+XAR7, 75F),
+            new PointF(17F+XAR7, 75F),
+            new PointF(10F+XAR7, 65F),
+            new PointF(3F+XAR7, 75F),
+            new PointF(0F+XAR7, 75F),
+            new PointF(3F+XAR7, 75F),
+
+
+
+        };
+
+            PointF[] arrowpoints72 = //МАССИВ ДЛЯ ПОСТРОЕНИЯ "2 ОПОРЫ(2)" 
+{
+            new PointF(20F+XAR7, 77F),
+            new PointF(0F+XAR7, 77F),
+            new PointF(3F+XAR7, 77F),
+            new PointF(4F+XAR7, 82F),
+            new PointF(5F+XAR7, 77F),
+            new PointF(7F+XAR7, 77F),
+            new PointF(6F+XAR7, 82F),
+            new PointF(7F+XAR7, 77F),
+            new PointF(9F+XAR7, 77F),
+            new PointF(8F+XAR7, 82F),
+            new PointF(9F+XAR7, 77F),
+            new PointF(11F+XAR7, 82F),
+            new PointF(10F+XAR7, 82F),
+            new PointF(11F+XAR7, 77F),
+            new PointF(13F+XAR7, 77F),
+            new PointF(12F+XAR7, 82F),
+            new PointF(13F+XAR7, 77F),
+            new PointF(15F+XAR7, 77F),
+            new PointF(14F+XAR7, 82F)
+
+
+
+        };
+
+            Graphics gPanel = panel1.CreateGraphics(); //СОЗДАНИЕ ГРАФИЧЕСКОГО ИНТЕРФЕЙСЯ В ЭЛЕМЕНТЕ ПАНЕЛЬ
+            gPanel.DrawLine(p = new Pen(Color.Gray, 3), new Point(10, 65), new Point(MaxL, 65)); //БАЛКА ДЛИННОЙ В 650 ЕДuНИЦ, ЛИБО В 8 МЕТРОВ В МАСШТАБЕ
+            gPanel.DrawLine(p = new Pen(Color.Gray, 1), new Point(10, 140), new Point(MaxL, 140)); //РАЗМЕРЫ БАЛКИ
+            gPanel.DrawLine(p = new Pen(Color.Red, 1), new Point(10, 0), new Point(10 + MaxF, 0)); //РАЗМЕРЫ СТРЕЛКИ СИЛЫ
+            gPanel.DrawLine(p = new Pen(Color.Green, 1), new Point(10, 115), new Point(10 + MaxF2, 115)); //РАЗМЕРЫ ВТОРОЙ СТРЕЛКИ СИЛЫ
+            gPanel.DrawLine(p = new Pen(Color.Blue, 1), new Point(10, 15), new Point(10 + MaxM, 15)); //РАЗМЕРЫ СИЛЫ МОМЕНТА
+            gPanel.DrawLine(p = new Pen(Color.Purple, 1), new Point(10, 30), new Point(10 + MaxM2, 30)); //РАЗМЕРЫ СИЛЫ МОЕНТА (2)
+            gPanel.DrawLine(p = new Pen(Color.SaddleBrown, 1), new Point(10, 45), new Point(10 + MaxQ, 45)); //РАЗМЕРЫ НАГРУЗКИ
+            //gPanel.DrawLine(p = new Pen(Color.Red,1), new Point(655, 0), new Point(655, 130)); //ПРОВЕРКА ГРАНИЦ РАМКИ
+            gPanel.DrawLine(p, new Point(arrow[4], arrow[5]), new Point(arrow[6], arrow[7])); //ПОСТРОЕНИЕ ЛИНИИ ПО ДАННЫМ ИЗ МАССИВА
+            //gPanel.DrawLine(p, new Point(arrow[8], arrow[9]), new Point(arrow[10], arrow[11]));// //ПОСТРОЕНИЕ ЛИНИИ ПО ДАННЫМ ИЗ МАССИВА
+            gPanel.DrawLines(p = new Pen(Color.Red, 3), arrowpoints); //ПОСТРОЕНИЕ СТРЕЛКИ ОДНОЙ КРИВОЙ
+            gPanel.DrawLines(p = new Pen(Color.Green, 3), arrowpoints2); //ПОСТРОЕНИЕ ВТОРОЙ СТРЕЛКИ ОДНОЙ КРИВОЙ
+            gPanel.DrawLines(p = new Pen(Color.Blue, 3), arrowpoints3); //ПОСТРОЕНИЕ СИЛЫ МОМЕНТА ОДНОЙ КРИВОЙ
+            gPanel.DrawLines(p = new Pen(Color.Purple, 3), arrowpoints4); //ПОСТРОЕНИЕ (2)СИЛЫ МОМЕНТА ОДНОЙ КРИВОЙ
+            gPanel.DrawLines(p = new Pen(Color.SaddleBrown, 3), arrowpoints5); //ПОСТРОЕНИЕ Q ОДНОЙ КРИВОЙ
+            gPanel.DrawLines(p = new Pen(Color.Gray, 1), arrowpoints6); //ПОСТРОЕНИЕ ОПОРЫ ОДНОЙ КРИВОЙ
+            gPanel.DrawLines(p = new Pen(Color.Gray, 1), arrowpoints71); //ПОСТРОЕНИЕ 2 ОПОРЫ ОДНОЙ КРИВОЙ
+            gPanel.DrawLines(p = new Pen(Color.Gray, 1), arrowpoints72); //ПОСТРОЕНИЕ 2 ОПОРЫ(2) ОДНОЙ КРИВОЙ
+            Graphics gButton = button1.CreateGraphics(); //МАКРОС ДЛЯ КНОПКИ
+
+
+            int[] arrow2 = { 10, 0, 10, 20, 10, 20, 5, 15, 10, 20, 15, 15 }; //ИНИЦИАЛИЗАЦИЯ МАССИВА ДАННЫХ ДЛЯ ЛИНИЙ
+            label1.Text = Convert.ToString(MaxL / 80); //СТОРКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА БАЛКИ
+            label1.Location = new System.Drawing.Point(6 + MaxL / 2, 145); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР БАЛКИ
+            label1.ForeColor = Color.Gray; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР БАЛКИ
+
+            label2.Text = Convert.ToString(MaxF / 80); //СТРОКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА СТРЕЛКИ СИЛЫ
+            label2.Location = new System.Drawing.Point(10 + MaxF / 2, 5); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР СТРЕЛКИ СИЛЫ
+            label2.ForeColor = Color.Red; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР СТРЕЛКИ СИЛЫ
+
+            label3.Text = Convert.ToString(MaxF2 / 80); //СТРОКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА ВТОРОЙ СТРЕЛКИ СИЛЫ
+            label3.Location = new System.Drawing.Point(10 + MaxF2 / 2, 115); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ВТОРОЙ СТРЕЛКИ СИЛЫ
+            label3.ForeColor = Color.Green; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ВТОРОЙ СТРЕЛКИ СИЛЫ
+
+            label4.Text = Convert.ToString(MaxM / 80); //СТРОКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА МОМЕНТА СИЛЫ
+            label4.Location = new System.Drawing.Point(9 + MaxM / 2, 20); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР МОМЕНТА СИЛЫ
+            label4.ForeColor = Color.Blue; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР МОМЕНТА СИЛЫ
+
+            label5.Text = Convert.ToString(MaxM2 / 80); //СТРОКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА МОМЕНТА СИЛЫ (2)
+            label5.Location = new System.Drawing.Point(9 + MaxM2 / 2, 35); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР МОМЕНТА СИЛЫ (2)
+            label5.ForeColor = Color.Purple; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР МОМЕНТА СИЛЫ (2)
+
+            label6.Text = Convert.ToString(MaxQ / 80); //СТРОКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА НАГРУЗКИ
+            label6.Location = new System.Drawing.Point(9 + MaxQ / 2, 42); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР НАГРУЗКИ
+            label6.ForeColor = Color.SaddleBrown; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР НАГРУЗКИ
+
+            label7.Text = Convert.ToString(F / 80); //СТРОКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА ЗНАЧЕНИЯ СТРЕЛКИ СИЛЫ
+            label7.Location = new System.Drawing.Point(33 + MaxF, 5); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ЗНАЧЕНИЯ СТРЕЛКИ СИЛЫ
+            label7.ForeColor = Color.Red; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ЗНАЧЕНИЯ СТРЕЛКИ СИЛЫ
+
+            label8.Text = Convert.ToString(F2 / 80); //СТРОКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА ЗНАЧЕНИЯ ВТОРОЙ СТРЕЛКИ СИЛЫ
+            label8.Location = new System.Drawing.Point(33 + MaxF2, 115); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ЗНАЧЕНИЯ ВТОРОЙ СТРЕЛКИ СИЛЫ
+            label8.ForeColor = Color.Green; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ЗНАЧЕНИЯ ВТОРОЙ СТРЕЛКИ СИЛЫ
+
+            label9.Text = Convert.ToString(M / 80); //СТРОКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА ЗНАЧЕНИЯ МОМЕНТА СИЛЫ
+            label9.Location = new System.Drawing.Point(43 + MaxM, 20); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ЗНАЧЕНИЯ МОМЕНТА СИЛЫ
+            label9.ForeColor = Color.Blue; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ЗНАЧЕНИЯ МОМЕНТА СИЛЫ
+
+            label10.Text = Convert.ToString(M2 / 80); //СТРОКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА ЗНАЧЕНИЯ МОМЕНТА СИЛЫ (2)
+            label10.Location = new System.Drawing.Point(43 + MaxM2, 35); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ЗНАЧЕНИЯ МОМЕНТА СИЛЫ (2)
+            label10.ForeColor = Color.Purple; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ЗНАЧЕНИЯ МОМЕНТА СИЛЫ (2)
+
+            label11.Text = Convert.ToString(Q / 80); //СТРОКА ДЛЯ НАХОЖДЕНИЯ РАЗМЕРА ЗНАЧЕНИЯ НАГРУЗКИ
+            label11.Location = new System.Drawing.Point(33 + MaxQ, 55); //КООРДИНАТЫ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ЗНАЧЕНИЯ НАГРУЗКИ
+            label11.ForeColor = Color.SaddleBrown; //ЦВЕТ ЧИСЛА, КОТОРОЕ ОБОЗНАЧАЕТ РАЗМЕР ЗНАЧЕНИЯ НАГРУЗКИ
+
+
+            if (label2.Text == label6.Text) label6.Hide(); //КОМАНДА, КОТОРАЯ УДАЛЯЕТ ОДИНАКОВЫЕ РАЗМЕРЫ
+            if (label3.Text == label6.Text) label6.Hide(); //КОМАНДА, КОТОРАЯ УДАЛЯЕТ ОДИНАКОВЫЕ РАЗМЕРЫ
+            if (label4.Text == label6.Text) label6.Hide(); //КОМАНДА, КОТОРАЯ УДАЛЯЕТ ОДИНАКОВЫЕ РАЗМЕРЫ
+            if (label5.Text == label6.Text) label6.Hide(); //КОМАНДА, КОТОРАЯ УДАЛЯЕТ ОДИНАКОВЫЕ РАЗМЕРЫ
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            arrowdrow();
+        }
+    }
 }
