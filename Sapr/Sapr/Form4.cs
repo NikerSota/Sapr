f104e7253709941fa06cdb510ff34555748b3b58
NikerSota <39484103+NikerSota@users.noreply.github.com>
2018-06-07 15:03:27 +0300

    Revert "Обнова (до-альфа)"
    This reverts commit b4e4eaa74e0150c949025cfc5e4837734749f104.

--- Sapr/Sapr/Form4.cs
+++ Sapr/Sapr/Form4.cs
@@ -74,39 +74,36 @@ namespace Sapr
 
 		double a=0, b=0, c=0;
 		char single;
-		//double formula;
-		bool dat = false;
 		private void button14_Click(object sender, EventArgs e)
 		{
 				a = Convert.ToDouble(textBox1.Text);
 				single = '+';
-		//	if (dat == true)
 				textBox1.Clear();
+
 		}
 		private void button13_Click(object sender, EventArgs e)
 		{
 				a = Convert.ToDouble(textBox1.Text);
 				single = '*';
-			//if (dat == true)
 				textBox1.Clear();
 		}
+
 		private void button12_Click(object sender, EventArgs e)
 		{
 				a = Convert.ToDouble(textBox1.Text);
 				single = '-';
-			//if (dat == true)
 				textBox1.Clear();
 		}
 		private void button11_Click(object sender, EventArgs e)
 		{
+
 				a = Convert.ToDouble(textBox1.Text);
 				single = '/';
-			//if(dat==true)
 				textBox1.Clear();
 		}
 		private void button15_Click(object sender, EventArgs e)
 		{
-			//dat = true;
+
 			b = Convert.ToDouble(textBox1.Text);
 			switch (single)
 			{
@@ -120,7 +117,8 @@ namespace Sapr
 						c = a / b;
 						break;
 			}
-				textBox1.Text = c.ToString();	
+				textBox1.Text = c.ToString();
+			
 		}
 		private void button16_Click(object sender, EventArgs e)
 		{
@@ -138,5 +136,6 @@ namespace Sapr
 					textBox1.Text = textBox1.Text.Remove(0, 1);
 				else textBox1.Text = '-' + textBox1.Text;
 		}
+
 	}
 }
